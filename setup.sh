8b6a17e619f5168b79f2ff2710acd5fbb11949d1
Mirza Ali Murtuza Ahmadi <mamahmadi727@gmail.com>
2026-02-28 13:35:21 -0500

    Modify pip install commands and update download links
    Updated package installation commands and changed download URLs for TE Sif files.

--- setup.sh
+++ setup.sh
@@ -23,7 +23,7 @@ load_module hmmer
 
 if command -v python3 &> /dev/null; then
     python3 -m pip install --upgrade pip
-    python3 -m pip install pandas biopython numpy scikit-learn joblib imbalanced-learn matplotlib seaborn
+    python3 -m pip install pandas biopython numpy matplotlib seaborn joblib "scikit-learn==1.3.0" "imbalanced-learn==0.13.0"
 fi
 
 if command -v R &> /dev/null; then
@@ -61,10 +61,10 @@ wget https://zenodo.org/records/18777048/files/training_dataset.CSV?download=1
 
 # Download TE Sif Files
 mkdir -p TE_pipeline_sif_files
-wget -O TE_pipeline_sif_files/annosine_v2.sif $base_url/annosine_v2.sif?download=1
-wget -O TE_pipeline_sif_files/earlgrey_v5.1.sif $base_url/earlgrey_v5.1.sif?download=1
-wget -O TE_pipeline_sif_files/heliano.sif $base_url/heliano.sif?download=1
-wget -O TE_pipeline_sif_files/HiTE_V3.3.3.sif $base_url/HiTE_V3.3.3.sif?download=1
+wget -O TE_pipeline_sif_files/annosine_v2.sif https://zenodo.org/records/18777146/files/annosine_v2.sif?download=1
+wget -O TE_pipeline_sif_files/earlgrey_v5.1.sif https://zenodo.org/records/18777146/files/earlgrey_v5.1.sif?download=1
+wget -O TE_pipeline_sif_files/heliano.sif https://zenodo.org/records/18777146/files/heliano.sif?download=1
+wget -O TE_pipeline_sif_files/HiTE_V3.3.3.sif https://zenodo.org/records/18777146/files/HiTE_V3.3.3.sif?download=1
 
 
 
